26387c49f7704a30f420d64d070b90e0518338d4
chandrakiranambati <52145854+chandrakiranambati@users.noreply.github.com>
2022-03-30 08:22:22 +0530

    Add files via upload

---
+++ task-1.js
@@ -0,0 +1,23 @@
+let myresume={
+    "Personal details" :{
+        "name":"chandrakiran",
+        "Email id":"ambatichandrakiran17@gmail.com",
+        "phone number":"7286849771",
+    }
+    "Educational details":{
+        "School":"Ravindra Bharathi",
+        "10th marks":"8.8 CGPA",
+        "Intermediate":"TLMSM jr college",
+        "Inter marks":"922",
+        "B-Tech":"KLU",
+        "Branch":"EEE",
+        "CGPA":"7.77 CGPA",
+    }
+    "Professional Details":{
+        "Company":"IBM",
+        "Position":"Advanced system analyst",
+        "Join Date":"04-01-2021",
+    }
+    ""
+}
+console.log(myResume);
